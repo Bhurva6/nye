d88fb826b2e40c0edad6aca1ed2b5456b3d8536d
bhurva <bhurvaxsharma.india@gmail.com>
2023-12-29 23:38:50 +0530

    minor bug fixes

--- src/Components/AllResolutions.js
+++ src/Components/AllResolutions.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ResolutionBox from './ResolutionBox';
 
 const AllResolutions = ({ resolutions }) => {
-    if (!resolutions || resolutions.length === 0) {
-      return <p>No resolutions yet.</p>;
-    }
-    return (
-      <div className="container">
-        {resolutions.map((res, index) => (
-          <ResolutionBox key={index} resolution={res.text} name={res.name} />
-        ))}
-      </div>
-    );
-  };
+  return (
+    <div className="container">
+      {resolutions.map((res, index) => (
+        <ResolutionBox key={index} resolution={res.resolution} name={res.name} />
+      ))}
+    </div>
+  );
+};
 
 export default AllResolutions;
 
