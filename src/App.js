55e78b92aee18b8ea99791ca9381237ef4bc07ef
Amitabh Mishra <115503862+iamitabh@users.noreply.github.com>
2023-12-29 19:27:02 +0530

    Update App.js and Added firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import InputForm from './Components/InputForm';
-import ResolutionBox from './Components/ResolutionBox';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import AllResolutions from './Components/AllResolutions'; // New component for displaying all resolutions
 import './styles.css';
 
@@ -16,16 +17,37 @@ const Navbar = () => {
 
 
 // ye local storage method hai  - temporary - implement firebase here
+const firebaseConfig = {
+  apiKey: "AIzaSyBbyCdmxeoN8fyE9l89iyCQCJEPvPmI7GU",
+  authDomain: "new-year-resolution-21fdd.firebaseapp.com",
+  projectId: "new-year-resolution-21fdd",
+  storageBucket: "new-year-resolution-21fdd.appspot.com",
+  messagingSenderId: "815408232312",
+  appId: "1:815408232312:web:19aebdf01c47ceaf227a8d"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+const colRef = collection(db, 'resolutions');
 
 const App = () => {
   // Load resolutions from local storage on initial load
-  const storedResolutions = JSON.parse(localStorage.getItem('resolutions')) || [];
   const [resolutions, setResolutions] = useState(storedResolutions);
 
   useEffect(() => {
-    // Save resolutions to local storage whenever it changes
-    localStorage.setItem('resolutions', JSON.stringify(resolutions));
-  }, [resolutions]);
+    // Fetch data from Firebase and update state
+    const fetchData = async () => {
+      try {
+        const snapshot = await getDocs(colRef);
+        const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        setResolutions(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const addResolution = (newResolution) => {
     setResolutions([...resolutions, newResolution]);
