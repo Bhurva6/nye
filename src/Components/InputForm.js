0a1269ae173fde3e29586b1a5ccf2a395e2dc2c5
bhurva <bhurvaxsharma.india@gmail.com>
2023-12-30 20:09:08 +0530

    minor changes

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -4,20 +4,27 @@ import { db } from '../App.js'; // Import your Firebase configuration
 
 const InputForm = () => {
   const [text, setText] = useState('');
-  const [name, setName] = useState('');
+  const [isPosted, setIsPosted] = useState(false);
+  //const [name, setName] = useState('');
 
   const handlePost = async () => {
-    if (text && name) {
+    if (text) {
+        
       // Add the document to Firestore
       try {
+        
         const docRef = await addDoc(collection(db, 'resolutions'), {
           resolution: text,
-          name,
+          //name,
         });
+        setText('');
+        setIsPosted(true);
+        setTimeout(() => {
+            setIsPosted(false);
+          }, 3000);
 
         // Clear the input fields
-        setText('');
-        setName('');
+        //setName('');
       } catch (e) {
         console.error('Error adding document: ', e);
       }
@@ -41,6 +48,7 @@ const InputForm = () => {
             paddingRight:40,
             paddingLeft:40,
             marginBottom:40,
+            fontWeight:'bold'
 
         }}>This is an anonymous website to put in your "new years goal" for 2024 that you are going to set for yourself and so will millions of others that you can check out too. This is where the world will put their silly, personal (not reccomended) and cool things that they want to do in 2024. Happy New Years :) </div>
       <input
@@ -50,7 +58,7 @@ const InputForm = () => {
   onChange={(e) => setText(e.target.value)}
   style={{
     width: '900px',
-    height: '400px',
+    height: '250px',
     padding: '10px',
     border: '0px solid transparent',
     borderRadius: '5px',
@@ -59,7 +67,8 @@ const InputForm = () => {
     margin: 'auto',
     display: 'block',
     position: 'relative',
-    textAlign: 'left', // Align text to the left
+    textAlign: 'left', 
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
   }}
 />
 <style>
@@ -79,19 +88,27 @@ const InputForm = () => {
 </style>
 
 <div style={{paddingBottom:30}}></div>
-<button style={{
-  marginTop: "30px",
-  width: "80px",
-  height: "40px",
-  borderRadius: "5px",
-  textAlign: "center",
-  display: "block", 
-  margin: "0 auto", 
-  
-}}
-onClick={handlePost}>
-    Post
-    </button>
+
+<button
+        style={{
+          marginTop: '30px',
+          width: '80px',
+          height: '40px',
+          borderRadius: '5px',
+          textAlign: 'center',
+          display: 'block',
+          margin: '0 auto',
+          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+        }}
+        onClick={handlePost}
+      >
+        Post
+      </button>
+      {isPosted && (
+        <div style={{ textAlign: 'center', marginTop: '20px', color: 'green', fontWeight:'bold', fontSize:'24' }}>
+          Your resolution is now visible to the world ! 
+        </div>
+      )}
     </div>
   );
 };
